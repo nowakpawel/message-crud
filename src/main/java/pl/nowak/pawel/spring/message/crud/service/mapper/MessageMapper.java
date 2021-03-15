69eff7e8390a13499c558c5e7011eaa86d57503d
Paweł Nowak <kontakt.nowakpawel@gmail.com>
2021-03-15 23:55:23 +0100

    MessageMapper implementation

--- src/main/java/pl/nowak/pawel/spring/message/crud/service/mapper/MessageMapper.java
+++ src/main/java/pl/nowak/pawel/spring/message/crud/service/mapper/MessageMapper.java
@@ -1,5 +1,6 @@
 package pl.nowak.pawel.spring.message.crud.service.mapper;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 import pl.nowak.pawel.spring.message.crud.repository.entity.MessageEntity;
 import pl.nowak.pawel.spring.message.crud.web.model.MessageModel;
@@ -7,7 +8,15 @@ import pl.nowak.pawel.spring.message.crud.web.model.MessageModel;
 @Component
 public class MessageMapper {
     public MessageEntity from(MessageModel messageModel) {
-        return null;
+        ModelMapper modelMapper = new ModelMapper();
+        MessageEntity messageEntity = modelMapper.map(messageModel, MessageEntity.class);
+        return messageEntity;
+    }
+
+    public MessageModel from(MessageEntity messageEntity) {
+        ModelMapper modelMapper = new ModelMapper();
+        MessageModel messageModel = modelMapper.map(messageEntity, MessageModel.class);
+        return messageModel;
     }
 
 }

dcc026f2c0a1c5f4b3fa184070b76a5e83738d37
Paweł Nowak <kontakt.nowakpawel@gmail.com>
2021-03-16 11:57:00 +0100

    Add logger implementation

--- src/main/java/pl/nowak/pawel/spring/message/crud/service/MessageService.java
+++ src/main/java/pl/nowak/pawel/spring/message/crud/service/MessageService.java
@@ -6,20 +6,31 @@ import pl.nowak.pawel.spring.message.crud.repository.entity.MessageEntity;
 import pl.nowak.pawel.spring.message.crud.service.mapper.MessageMapper;
 import pl.nowak.pawel.spring.message.crud.web.model.MessageModel;
 
+import java.util.logging.Logger;
+
 @Service
 public class MessageService {
+    private final static Logger logger = Logger.getLogger(MessageService.class.getName());
 
     private MessageMapper messageMapper;
     private MessageRepository messageRepository;
 
     public MessageService(MessageMapper messageMapper, MessageRepository messageRepository) {
+        logger.info("(MessageService::constructor)   ====>   this.messageMapper = " + this.messageMapper);
+        logger.info("(MessageService::constructor)   ====>   this.messageRepository = " + this.messageRepository);
+
         this.messageMapper = messageMapper;
         this.messageRepository = messageRepository;
+
+        logger.info("(MessageService::constructor)   ====>   this.messageMapper = " + this.messageMapper);
+        logger.info("(MessageService::constructor)   ====>   this.messageRepository = " + this.messageRepository);
     }
 
     public MessageModel create(MessageModel messageModel) {
+        logger.info("(MessageService::create)   ====>   Create MessageEntity object");
         MessageEntity messageEntity = messageMapper.from(messageModel);
         MessageEntity savedMessageEntity = messageRepository.save(messageEntity);
+        logger.info("(MessageService::create)   ====>   Returned MessageEntity object: " + messageEntity);
         return null;
     }
 
--- src/main/java/pl/nowak/pawel/spring/message/crud/service/mapper/MessageMapper.java
+++ src/main/java/pl/nowak/pawel/spring/message/crud/service/mapper/MessageMapper.java
@@ -5,17 +5,29 @@ import org.springframework.stereotype.Component;
 import pl.nowak.pawel.spring.message.crud.repository.entity.MessageEntity;
 import pl.nowak.pawel.spring.message.crud.web.model.MessageModel;
 
+import java.util.logging.Logger;
+
 @Component
 public class MessageMapper {
+    private final static Logger logger = Logger.getLogger(MessageMapper.class.getName());
+
     public MessageEntity from(MessageModel messageModel) {
+        logger.info("(MessageMapper::from(MessageModel)    ====>    create MessageEntity from MessageModel)");
+
         ModelMapper modelMapper = new ModelMapper();
         MessageEntity messageEntity = modelMapper.map(messageModel, MessageEntity.class);
+
+        logger.info("(MessageMapper::from(MessageModel)    ====>    returned messageEntity = " + messageEntity + "");
         return messageEntity;
     }
 
     public MessageModel from(MessageEntity messageEntity) {
+        logger.info("(MessageMapper::from(MessageEntity)    ====>    create MessageModel from MessageEntity)");
+
         ModelMapper modelMapper = new ModelMapper();
         MessageModel messageModel = modelMapper.map(messageEntity, MessageModel.class);
+
+        logger.info("(MessageMapper::from(MessageEntity)    ====>    returned messageModel = " + messageModel + "");
         return messageModel;
     }
 
